350630c8d8419e531080eeee5b57c52bee663686
Cody <ctappan@axon.com>
2018-10-09 17:33:05 -0700

    Working resize handler, display. Still need to stuff to 32 bit boundary for qt view

--- src/image/bitmap.h
+++ src/image/bitmap.h
@@ -1,3 +1,18 @@
-class Bitmapper {
-    
+#ifndef BITMAP_H
+#define BITMAP_H
+
+#include <memory>
+#include <vector>
+
+class Bitmap {
+public:
+    Bitmap(int width, int height);
+    ~Bitmap() = default;
+
+    std::vector<unsigned char>* GetImageData() { return mImageData.get(); }
+
+private:
+    std::unique_ptr<std::vector<unsigned char>> mImageData;
 };
+
+#endif
--- src/qt/mainwindow.cpp
+++ src/qt/mainwindow.cpp
@@ -1,14 +1,50 @@
-#include "include/mainwindow.h"
-#include "ui_mainwindow.h"
+#include "mainwindow.h"
+#include "ResizeHandler.h"
+
+#include "../image/bitmap.h"
+
+#include <iostream>
+
+using namespace std;
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
-    ui(new Ui::MainWindow)
+    view(new QGraphicsView)
 {
-    ui->setupUi(this);
+    this->setCentralWidget(view);
+
+    view->setScene(new QGraphicsScene);
+
+    // Install resize handler
+    ResizeHandler *handler = new ResizeHandler(this);
+    QCoreApplication::instance()->installEventFilter(handler);
+
+    connect(handler, SIGNAL(resized(int, int)), this, SLOT(UpdateImage(int, int)));
 }
 
 MainWindow::~MainWindow()
 {
-    delete ui;
+    delete view;
 }
+
+void MainWindow::UpdateImage(int width, int height) {
+    cout << "Updating image\n";
+    cout << "Width: " << width << "\n";
+    cout << "Height: " << height << "\n";
+
+    cout << "Gen bitmap\n";
+    Bitmap bitmap(width, height);
+
+    cout << "Populate img\n";
+    QImage img(bitmap.GetImageData()->data(), width, height, QImage::Format_Indexed8);
+
+    cout << "Clear scene\n";
+    view->scene()->clear();
+
+    cout << "Add img\n";
+    view->scene()->addPixmap(QPixmap::fromImage(img));
+
+    cout << "Update view\n";
+    view->update();
+}
+
